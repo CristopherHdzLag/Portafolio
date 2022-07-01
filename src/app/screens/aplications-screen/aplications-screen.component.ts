012c95c28e668fd6f8f242eb02fe5b04e49afd10
Cristopher Hernandez Lagunas <chl.cristopherhdz@gmail.com>
2022-07-01 18:59:29 -0500

    new build 01-07-2022-4 fix src from image

--- src/app/screens/aplications-screen/aplications-screen.component.ts
+++ src/app/screens/aplications-screen/aplications-screen.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AplicationsScreenComponent implements OnInit {
 
-  constructor() { }
+  logo : string = "https://cristopherhdzlag.github.io/Portafolio/assets/icono_1024.jpg";
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
